91b16316fb3a91ba8d528f62e627e770f82943eb
AlexanderInUM <alexander.um.edu@gmail.com>
2022-10-10 18:49:13 -0700

    hello-world

---
+++ hellospace/build/hellopack/CMakeFiles/print_hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/alexander/Documents/Projects/LearnROS/hellospace/devel/lib/hellopack/print_hello"
+  "/home/alexander/Documents/Projects/LearnROS/hellospace/devel/lib/hellopack/print_hello.pdb"
+  "CMakeFiles/print_hello.dir/src/print_hello.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/print_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
